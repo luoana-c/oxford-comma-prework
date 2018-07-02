f6dc651d9aebffc6bacfc55226529d9cea3630ff
Luoana Chirita <luoana.livadariu@gmail.com>
2018-07-02 08:42:46 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,11 @@
 def oxford_comma(array)
-  last = array.pop
-  string = array.join(", ") + ", and #{last}"
-  string
+  if array.length > 2
+    last = array.pop
+    string = array.join(", ") + ", and #{last}"
+    string
+  elsif array.length = 1 
+    string = "#{array[0]}"
+  elsif array.length = 2 
+    string = array.join(" and ")
+    
 end
