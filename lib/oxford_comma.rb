657a488724693b9f46e582fbf54d3fed47743e42
Luoana Chirita <luoana.livadariu@gmail.com>
2018-07-02 08:44:07 +0000

    oxford comma

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length > 2
+    last = array.pop
+    string = array.join(", ") + ", and #{last}"
+    string
+  elsif array.length == 1 
+    string = "#{array[0]}"
+  elsif array.length == 2 
+    string = array.join(" and ")
+    string 
+  end 
 end
